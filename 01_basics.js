a3389fd20ddc7d6732996ba3bed93ce9880e98fb
maaz266 <mma26june@gmail.com>
2025-01-31 15:39:47 +0530

    day1

---
+++ 01_basics.js
@@ -0,0 +1,19 @@
+const accountID= 822513
+let accountEmail= "HuiHui@gmail.com"
+let accountPassword="password"
+
+let accountCity="Pagalpur"
+let accountState;
+//accountID =4  //NOT ALLOWED
+
+accountEmail="uiu7t"
+accountPassword="5535"
+accountCity="jaipur"
+
+console.log(accountEmail);
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var coz beacuse of issue in blockspace and 
+functional scope
+*/
