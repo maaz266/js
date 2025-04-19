c66ce1f80c9c38db7e1b2b25d0853c8ba48ad631
maaz266 <mma26june@gmail.com>
2025-04-19 22:54:52 +0530

    conversionOf datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict "; //treat all js code as newer version
+//alert (3+3) we r using node and not browser
+console.log("Meow")
+
+let name = "Meow"
+let age = 22
+let isLoggedIn = true
+let state 
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// symbol =>unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
