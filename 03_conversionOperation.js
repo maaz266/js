4703b0f6282116171a3f356143b7bd7d32cb303e
maaz266 <mma26june@gmail.com>
2025-04-21 00:11:32 +0530

    datatype

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -25,3 +25,44 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//****************************** */
+//    OPERATIONS begin from here
+
+let value= 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(3%2);
+
+let str1 = "Hello";
+let str2 = " Meowaaz";
+
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( ( 3 + 4) * 5 % 3);
+
+console.log(+true);   //(true+)  =>  throws an error 
+console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
---
+++ datatypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types: Strings,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+
+console.log(id === anotherId);
+//const bigNumber = 45678901234567n;
+
+//Reference (Non Primitive)
+//array,objects, functions
+
+const heroes = ["Ironman" ,"CaptainAmerica" ,"BlackPanther"];
+let myObj = {
+    name:"Meowaaz",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+console.log(typeof  bigNumber);
+
+
+
+
