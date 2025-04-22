4b874c395cbef308b4f8f5189d9abad5d16f1bf8
maaz266 <mma26june@gmail.com>
2025-04-23 00:57:43 +0530

    Update datatypes_summary.js

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -30,6 +30,31 @@ const myFunction = function(){
 }
 console.log(typeof  bigNumber);
 
+// *******************************
+
+// Stack (Primitive) ,Heap (Non Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne;
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
 
 
 
